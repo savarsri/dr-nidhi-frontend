462f345edeea8039acaec92e31ccd8c68ff34261
Savar Srivastava <savarsri@gmail.com>
2025-03-13 20:49:51 +0530

    updated

--- src/api.js
+++ src/api.js
@@ -2,8 +2,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  // baseURL: 'http://127.0.0.1:8000',
-  baseURL: 'https://vivid-case-446615-j6.el.r.appspot.com/',
+  baseURL: 'http://127.0.0.1:8000',
+  // baseURL: 'https://vivid-case-446615-j6.el.r.appspot.com/',
   // timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
