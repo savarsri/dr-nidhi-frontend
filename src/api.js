6ef0f82343db1983b368eb8d12897dcfd1df4f41
Savar Srivastava <savarsri@gmail.com>
2025-06-21 01:08:14 +0530

    Revert API

--- src/api.js
+++ src/api.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://127.0.0.1:8000',
-  // baseURL: 'https://vivid-case-446615-j6.el.r.appspot.com/',
-  baseURL: 'http://34.100.158.180',
+  baseURL: 'https://vivid-case-446615-j6.el.r.appspot.com/',
+  // baseURL: 'http://34.100.158.180',
   // timeout: 5000,
   headers: {
     'Content-Type': 'application/json',
